2a65613e83fa172323a9bf8be89da633c214672a
harshagl2002 <79414830+harshagl2002@users.noreply.github.com>
2024-03-22 23:35:19 -0500

    Delete mp2 directory

--- mp2/see_results.sh
+++
@@ -1,8 +0,0 @@
-git checkout _grades -q
-git pull -q
-
-echo -e '\n-----------Contents of results.txt-----------------\n'
-cat results.txt
-echo -e '\n-----------End of results.txt----------------------\n'
-
-git checkout main -q
